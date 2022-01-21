321e79aa760bd4a0ca55bb8af5455793f6e1a628
Kim Stonehouse <kimbethstonehouse@gmail.com>
2022-01-21 17:17:09 +0000

    Renamed vdice scripts to rdice scrips

--- run-rdice.sh
+++ run-rdice.sh
@@ -6,7 +6,7 @@ INFOS_USER_DIR=$TOP/infos-user
 ROOTFS=$INFOS_USER_DIR/bin/rootfs.tar
 KERNEL=$INFOS_DIR/out/infos-kernel
 KERNEL_CMDLINE="boot-device=ata0 init=/usr/init pgalloc.debug=0 pgalloc.algorithm=simple objalloc.debug=0 sched.debug=0 sched.algorithm=cfs syslog=serial $*"
-QEMU_DIR=/afs/inf.ed.ac.uk/group/teaching/cs3/os/qemu
-QEMU_BIN=$QEMU_DIR/qemu-system-x86_64
+QEMU=qemu-system-x86_64
+PORT=`whoami | md5sum | cut -b 1-3 | awk '{print strtonum("0x"$1)}'`
 
-LD_LIBRARY_PATH=$QEMU_DIR $QEMU_BIN -L $QEMU_DIR -kernel $KERNEL -m 6G -debugcon stdio -hda $ROOTFS -append "$KERNEL_CMDLINE"
+$QEMU -display vnc=localhost:$PORT -kernel $KERNEL -m 6G -debugcon stdio -hda $ROOTFS -append "$KERNEL_CMDLINE"
